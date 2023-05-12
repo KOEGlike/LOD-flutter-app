551d57b9af2e7f38ed70955934346dea47d36e47
KOEGlike <KOEGlike@users.noreply.github.com>
2023-05-12 20:36:55 +0100

    small api get

--- lib/views/vote.dart
+++ lib/views/vote.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +12,21 @@ class Vote extends StatefulWidget {
 }
 
 class _VoteState extends State<Vote> {
+  late final List images;
+
+  Future<Map<String, String>> get(int id) async {
+    var url = Uri.https(
+        'example.com', 'whatsit/create', {"id": widget.id.toString()});
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    return Future;
+    jsonDecode(response);
+  }
+
+  void vote(int id, bool isyes) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
