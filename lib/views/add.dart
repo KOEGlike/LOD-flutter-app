09baa52714d0f9f8ff69e47fdec45bdc7d735ee4
KOEGlike <marcell.balazs@yahoo.com>
2023-05-22 12:52:33 +0200

    add.dart small steps

--- lib/views/add.dart
+++ lib/views/add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:file_selector/file_selector.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
@@ -8,11 +9,42 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  late final TextEditingController _controller;
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: const Text('add'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name Of LOD',
+                  ),
+                )),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add Photo'),
+            )
+          ],
+        ),
       ),
     );
   }
