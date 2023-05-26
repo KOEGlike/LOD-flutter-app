2f564d6d695767c974194e70b9f76300fe2e80a1
KOEGlike <KOEGlike@users.noreply.github.com>
2023-05-26 14:20:38 +0100

    imported tinder pacage and started tehe migration

--- lib/views/vote.dart
+++ lib/views/vote.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:first_test/views/results.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:swipe_cards/swipe_cards.dart';
 
 class Vote extends StatefulWidget {
   final int id;
@@ -13,19 +14,9 @@ class Vote extends StatefulWidget {
 
 class _VoteState extends State<Vote> with TickerProviderStateMixin {
   late final Future<Map<String, dynamic>> response;
-  late AnimationController _linearAnimationController;
-  late AnimationController _rotationAnimationController;
-  late double fullslide;
+
   double _animationProcantage = 0;
   int currentIndex = 0;
-
-  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: Offset.zero,
-    end: const Offset(1.5, 0.0),
-  ).animate(CurvedAnimation(
-    parent: _linearAnimationController,
-    curve: Curves.linear,
-  ));
 
   Future<Map<String, dynamic>> get(int id) async {
     Uri url = Uri.https('koeg.000webhostapp.com', 'sop/api.php/get',
@@ -61,20 +52,12 @@ class _VoteState extends State<Vote> with TickerProviderStateMixin {
 
   @override
   void initState() {
-    fullslide = MediaQuery.of(context).size.width / 4;
-    _linearAnimationController = AnimationController(vsync: this);
-    _rotationAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(microseconds: 500),
-    );
     response = get(widget.id);
     super.initState();
   }
 
   @override
   void dispose() {
-    _linearAnimationController.dispose();
-    _rotationAnimationController.dispose();
     super.dispose();
   }
 
@@ -95,65 +78,40 @@ class _VoteState extends State<Vote> with TickerProviderStateMixin {
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      onHorizontalDragStart: (details) {
-                        setState(() {
-                          _animationProcantage = 0;
-                        });
+                    child: Image.network(
+                      'http://koeg.000webhostapp.com/sop/images/$id/${images[currentIndex]['file_Name']}',
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.height * 0.7,
+                      height: null,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        int? expecdtByts = loadingProgress.expectedTotalBytes;
+                        int? currentByts =
+                            loadingProgress.cumulativeBytesLoaded;
+                        if (expecdtByts != null) {
+                          var loadingProcent = currentByts / expecdtByts;
+                          return Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height *
+                                    0.5 /
+                                    2,
+                                bottom: MediaQuery.of(context).size.height *
+                                    0.5 /
+                                    2,
+                              ),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                child: LinearProgressIndicator(
+                                  value: loadingProcent,
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          return child;
+                        }
                       },
-                      onHorizontalDragUpdate: (details) {
-                        setState(() {});
-                      },
-                      onHorizontalDragEnd: (details) {},
-                      onHorizontalDragCancel: () {
-                        setState(() {
-                          _animationProcantage = 0;
-                        });
-                      },
-                      child: SlideTransition(
-                        position: _offsetAnimation,
-                        child: RotationTransition(
-                          turns: _rotationAnimationController,
-                          child: Image.network(
-                            'http://koeg.000webhostapp.com/sop/images/$id/${images[currentIndex]['file_Name']}',
-                            fit: BoxFit.cover,
-                            width: MediaQuery.of(context).size.height * 0.7,
-                            height: null,
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress == null) return child;
-                              int? expecdtByts =
-                                  loadingProgress.expectedTotalBytes;
-                              int? currentByts =
-                                  loadingProgress.cumulativeBytesLoaded;
-                              if (expecdtByts != null) {
-                                var loadingProcent = currentByts / expecdtByts;
-                                return Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.only(
-                                      top: MediaQuery.of(context).size.height *
-                                          0.5 /
-                                          2,
-                                      bottom:
-                                          MediaQuery.of(context).size.height *
-                                              0.5 /
-                                              2,
-                                    ),
-                                    child: SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.7,
-                                      child: LinearProgressIndicator(
-                                        value: loadingProcent,
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              } else {
-                                return child;
-                              }
-                            },
-                          ),
-                        ),
-                      ),
                     ),
                   ),
                   Padding(
