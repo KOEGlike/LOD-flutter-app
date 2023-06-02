909f1cc6a9c6da01622bdf6de191eb8cc5f02a50
KOEGlike <marcell.balazs@yahoo.com>
2023-06-02 19:50:01 +0200

    reverted to non gpt variant
    becouse it was not right for me

--- lib/views/vote.dart
+++ lib/views/vote.dart
@@ -61,13 +61,7 @@ class _VoteState extends State<Vote> {
     return FutureBuilder<Map<String, dynamic>>(
       future: response,
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else if (snapshot.hasData) {
+        if (snapshot.hasData) {
           final images = snapshot.data!['images'];
           final name = snapshot.data!['name'];
           final id = widget.id;
@@ -83,70 +77,71 @@ class _VoteState extends State<Vote> {
                     width: 500,
                     height: 500,
                     child: SwipeCards(
-                      upSwipeAllowed: false,
-                      fillSpace: false,
-                      matchEngine: _matchEngine,
-                      onStackFinished: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ResultsPage(id: id),
-                          ),
-                        );
-                      },
-                      itemBuilder: (context, index) {
-                        return Container(
-                          color: Theme.of(context).colorScheme.background,
-                          padding: const EdgeInsets.all(8.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              'http://koeg.000webhostapp.com/sop/images/$id/${images[index]['file_Name']}',
-                              fit: BoxFit.cover,
-                              width: MediaQuery.of(context).size.height * 0.7,
-                              height: null,
-                              loadingBuilder:
-                                  (context, child, loadingProgress) {
-                                if (loadingProgress == null) return child;
-                                int? expectedBytes =
-                                    loadingProgress.expectedTotalBytes;
-                                int? currentBytes =
-                                    loadingProgress.cumulativeBytesLoaded;
-                                if (expectedBytes != null) {
-                                  var loadingPercent =
-                                      currentBytes / expectedBytes;
+                        upSwipeAllowed: false,
+                        fillSpace: false,
+                        matchEngine: _matchEngine,
+                        onStackFinished: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ResultsPage(id: id),
+                            ),
+                          );
+                        },
+                        itemBuilder: (context, index) {
+                          return Container(
+                            color: Theme.of(context).colorScheme.background,
+                            padding: const EdgeInsets.all(8.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                'http://koeg.000webhostapp.com/sop/images/$id/${images[index]['file_Name']}',
+                                fit: BoxFit.cover,
+                                width: MediaQuery.of(context).size.height * 0.7,
+                                height: null,
+                                loadingBuilder:
+                                    (context, child, loadingProgress) {
+                                  if (loadingProgress == null) return child;
+                                  int? expecdtByts =
+                                      loadingProgress.expectedTotalBytes;
+                                  int? currentByts =
+                                      loadingProgress.cumulativeBytesLoaded;
+                                  if (expecdtByts != null) {
+                                    var loadingProcent =
+                                        currentByts / expecdtByts;
 
-                                  return Center(
-                                    child: Padding(
-                                      padding: EdgeInsets.only(
-                                        top:
-                                            MediaQuery.of(context).size.height *
+                                    return Center(
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                            top: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
                                                 0.5 /
                                                 2,
-                                        bottom:
-                                            MediaQuery.of(context).size.height *
+                                            bottom: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
                                                 0.5 /
-                                                2,
-                                      ),
-                                      child: SizedBox(
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                0.7,
-                                        child: LinearProgressIndicator(
-                                          value: loadingPercent,
+                                                2),
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.7,
+                                          child: LinearProgressIndicator(
+                                            value: loadingProcent,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  );
-                                } else {
-                                  return child;
-                                }
-                              },
+                                    );
+                                  } else {
+                                    return child;
+                                  }
+                                },
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
+                          );
+                        }),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 70.0),
@@ -155,9 +150,9 @@ class _VoteState extends State<Vote> {
                       children: [
                         Padding(
                           padding: EdgeInsets.only(
-                            right:
-                                MediaQuery.of(context).size.width * 0.7 / 2.5,
-                          ),
+                              right: MediaQuery.of(context).size.width *
+                                  0.7 /
+                                  2.5),
                           child: SizedBox(
                             width: 80,
                             height: 40,
@@ -186,6 +181,8 @@ class _VoteState extends State<Vote> {
               ),
             ),
           );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
         } else {
           return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
