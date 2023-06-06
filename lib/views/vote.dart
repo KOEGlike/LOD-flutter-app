96ea35be281b5702009d3721e8ed1dcf4dbb0898
KOEGlike <KOEGlike@users.noreply.github.com>
2023-06-06 09:39:54 +0300

    changed swiper package

--- lib/views/vote.dart
+++ lib/views/vote.dart
@@ -3,6 +3,7 @@ import 'package:first_test/views/results.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:swipe_cards/swipe_cards.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class Vote extends StatefulWidget {
   final int id;
@@ -14,8 +15,8 @@ class Vote extends StatefulWidget {
 
 class _VoteState extends State<Vote> {
   late Future<Map<String, dynamic>> response;
-  List<SwipeItem> _swipeItems = <SwipeItem>[];
-  late MatchEngine _matchEngine;
+
+  final CardSwiperController _controller = CardSwiperController();
 
   Future<Map<String, dynamic>> fetchImages(int id) async {
     Uri url = Uri.https('koeg.000webhostapp.com', 'sop/api.php/get',
@@ -35,25 +36,19 @@ class _VoteState extends State<Vote> {
   }
 
   List<SwipeItem> generateSwipeItems(int id, List<dynamic> images) {
-    return [
-      for (int i = 0; i < images.length; i++)
-        SwipeItem(
-          likeAction: () {
-            vote(int.parse(images[i]['id']), true, images);
-            debugPrint('yes');
-          },
-          nopeAction: () {
-            vote(int.parse(images[i]['id']), false, images);
-            debugPrint('no');
-          },
-        )
-    ];
+    return [for (int i = 0; i < images.length; i++) SwipeItem()];
   }
 
   @override
   void didChangeDependencies() {
     response = fetchImages(widget.id);
     super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -71,8 +66,7 @@ class _VoteState extends State<Vote> {
           final List<dynamic> images = snapshot.data!['images'];
           final String name = snapshot.data!['name'];
           final int id = widget.id;
-          _swipeItems = generateSwipeItems(id, images);
-          _matchEngine = MatchEngine(swipeItems: _swipeItems);
+
           debugPrint(jsonEncode(images));
           return Scaffold(
             appBar: AppBar(
@@ -86,153 +80,26 @@ class _VoteState extends State<Vote> {
             body: Center(
               child: Column(
                 children: [
-                  ImageSlide(),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 70.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            right:
-                                MediaQuery.of(context).size.width * 0.7 / 2.5,
-                          ),
-                          child: SizedBox(
-                            width: 80,
-                            height: 40,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                _matchEngine.currentItem?.nope();
-                              },
-                              child: const Icon(Icons.close),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 80,
-                          height: 40,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _matchEngine.currentItem?.like();
-                            },
-                            child: const Icon(Icons.done),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        } else {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-      },
-    );
-  }
-}
-
-class ImageSlide extends StatefulWidget {
-  const ImageSlide({Key? key}) : super(key: key);
-
-  @override
-  State<ImageSlide> createState() => _ImageSlideState();
-}
-
-class _ImageSlideState extends State<ImageSlide>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _positionAnimation;
-  late Animation<double> _rotationAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _positionAnimation = _controller.drive(Tween<double>(begin: 0.0, end: 0.0));
-    _rotationAnimation = _controller.drive(Tween<double>(begin: 0.0, end: 0.0));
-
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-
-    _positionAnimation =
-        Tween<double>(begin: 0.0, end: 0.0).animate(_controller)
-          ..addListener(() {
-            setState(() {});
-          });
-
-    _rotationAnimation =
-        Tween<double>(begin: 0.0, end: 0.0).animate(_controller)
-          ..addListener(() {
-            setState(() {});
-          });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onHorizontalDragUpdate: (DragUpdateDetails details) {
-        setState(() {
-          _positionAnimation = Tween<double>(
-            begin: _positionAnimation.value,
-            end: details.delta.dx / MediaQuery.of(context).size.width,
-          ).animate(_controller);
-
-          _rotationAnimation = Tween<double>(
-            begin: _rotationAnimation.value,
-            end: (_positionAnimation.value * 30.0),
-          ).animate(_controller);
-        });
-      },
-      onHorizontalDragEnd: (DragEndDetails details) {
-        _positionAnimation = Tween<double>(
-          begin: _positionAnimation.value,
-          end: 0.0,
-        ).animate(_controller);
-
-        _rotationAnimation = Tween<double>(
-          begin: _rotationAnimation.value,
-          end: 0.0,
-        ).animate(_controller);
-
-        _controller.reset();
-        _controller.forward();
-      },
-      child: Transform.translate(
-        offset: Offset(
-          (_positionAnimation.value * MediaQuery.of(context).size.width * 0.5),
-          MediaQuery.of(context).size.height / 2 - 50.0,
-        ),
-        child: Transform.rotate(
-          angle: _rotationAnimation.value * (3.14 / 180.0),
-          child: Container(
-            width: 100.0,
-            height: 100.0,
-            color: Colors.blue,
-          ),
-        ),
-      ),
-    );
-  }
-}
-/*SizedBox(
+                  SizedBox(
                     width: 500,
                     height: 500,
-                    child: SwipeCards(
-                      upSwipeAllowed: false,
-                      fillSpace: false,
-                      matchEngine: _matchEngine,
-                      onStackFinished: () {
+                    child: CardSwiper(
+                      cardsCount: images.length,
+                      controller: _controller,
+                      onSwipe: (previousIndex, currentIndex, direction) {
+                        if (direction == CardSwiperDirection.right) {
+                          vote(int.parse(images[currentIndex ?? 0]['id']), true,
+                              images);
+                          debugPrint('yes');
+                        } else {
+                          vote(int.parse(images[currentIndex ?? 0]['id']),
+                              false, images);
+                          debugPrint('no');
+                        }
+
+                        return true;
+                      },
+                      onEnd: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
@@ -240,7 +107,7 @@ class _ImageSlideState extends State<ImageSlide>
                           ),
                         );
                       },
-                      itemBuilder: (context, index) {
+                      cardBuilder: (context, index) {
                         return Container(
                           color: Colors.red,
                           padding: const EdgeInsets.all(8.0),
@@ -293,4 +160,51 @@ class _ImageSlideState extends State<ImageSlide>
                         );
                       },
                     ),
-                  ),*/
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 70.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            right:
+                                MediaQuery.of(context).size.width * 0.7 / 2.5,
+                          ),
+                          child: SizedBox(
+                            width: 80,
+                            height: 40,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                //_matchEngine.currentItem?.nope();
+                              },
+                              child: const Icon(Icons.close),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 80,
+                          height: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // _matchEngine.currentItem?.like();
+                            },
+                            child: const Icon(Icons.done),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
+    );
+  }
+}
