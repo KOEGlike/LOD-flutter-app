04f071ca846b9dcd66a4cbc7121e2d0be6f76161
KOEGlike <KOEGlike@users.noreply.github.com>
2023-07-07 12:58:58 +0300

    commented out unused import

--- lib/views/skeleton.dart
+++ lib/views/skeleton.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../custom_error.dart';
+//import '../custom_error.dart';
 
 class NavIcon {
   final Icon icon;
