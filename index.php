90229f15aeaaa9fa457cc1538e924210f6aa8f5e
Balázs Marcell <103954639+KOEGlike@users.noreply.github.com>
2023-07-15 23:00:08 +0300

    started to work on index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
+require __DIR__ . "/inc/bootstrap.php";
 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+str_replace(__DIR__, "", $uri);
+$uri = explode( '/', $uri );
+
+
+
+if($uri[0]==) 
 ?>
