abb9bb6623528831d8356b7d0b53079c859144b7
KOEGlike <marcell.balazs@yahoo.com>
2023-07-31 14:40:38 +0200

    clean up

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 require __DIR__ . "/inc/bootstrap.php";
 
-
 $uri = str_replace("/".explode( 'public_html/', __DIR__)[1]."/", "", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $uri = explode( '/', $uri );
 //$uri= array_shift($uri);
@@ -64,8 +63,4 @@ else{
 $baseController->thisEndpointDoseNotExist();
 }
 
-
-
-
- 
 ?>
